1e98cb539f227b30c4737da370da9363c3a4c41e
Tanya Zamorska <zamorskatetiana@gmail.com>
2016-12-15 20:21:03 +0200

    display the current time

--- public_html/main.js
+++ public_html/main.js
@@ -1,4 +1,28 @@
 "use strict";
 
+function initialClocks() {
+  var d = new Date();
+  var seconds = d.getSeconds();
+  var minutes = d.getMinutes();
+  var hours = d.getHours();
+  
+  var currentSeconds = document.querySelectorAll("div")[1];
+  var seconds60 = seconds % 60;
+  var secondsDegree = 360 * seconds60 / 360;
+  console.log(seconds);
+  currentSeconds.style.cssText = "transform: rotate("+secondsDegree+"deg);";
+  
+  var currentMinutes = document.querySelectorAll("div")[3];
+  var minutes60 = minutes % 60;
+  console.log(minutes);
+  var minutesDegree = 360 * minutes60 / 60;
+  currentMinutes.style.cssText = "transform: rotate("+minutesDegree+"deg);";
+  
+  var currentHour = document.querySelectorAll("div")[5];
+  var hours12 = hours % 12;
+  var hoursDegree = 360 * hours12 / 12;
+  currentHour.style.cssText = "transform: rotate("+hoursDegree+"deg);";	
+}
+initialClocks();
 
-
+  
